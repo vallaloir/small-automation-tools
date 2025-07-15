6bb20296bc56a0ec38a808c46a6f07f2ebbca284
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-07-15 21:48:46 +0300

    Add formatting and linting tools and format current files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
 
 # Exports the notebook as WASM to use locally or upload to GitHub Pages
-subprocess.run(["marimo", "export", "html-wasm", "tools.py", "-o", "tools", "--mode", "run"])
+subprocess.run(
+    ["marimo", "export", "html-wasm", "tools.py", "-o", "tools", "--mode", "run"]
+)
