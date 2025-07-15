e24b54c0508aebb133a1bddb8ffe1d7244d1ac73
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-07-15 21:24:43 +0300

    Initialise uv project with main exporting the WASM app

---
+++ main.py
@@ -0,0 +1,4 @@
+import subprocess
+
+# Exports the notebook as WASM to use locally or upload to GitHub Pages
+subprocess.run(["marimo", "export", "html-wasm", "tools.py", "-o", "tools", "--mode", "run"])
