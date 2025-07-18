254b681ad3741d2030fb06b9fc2b60d75cafbbf4
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-07-18 13:04:57 +0300

    Update the build command

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import subprocess
 
 # Exports the notebook as WASM to use locally or upload to GitHub Pages
 subprocess.run(
-    ["marimo", "export", "html-wasm", "tools.py", "-o", "tools", "--mode", "run"]
+    [
+        "uv",
+        "run",
+        "build.py",
+        "--output_dir",
+        "_site",
+        "--template",
+        "templates/tailwind.html.j2",
+    ]
 )
