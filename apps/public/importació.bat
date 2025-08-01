1f677fbcfc60c752381d5229c56980e940eb5237
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-08-01 08:49:24 +0300

    Add batch file to execute it easily on Windows

---
+++ apps/public/importació.bat
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+uv run https://vallaloir.github.io/small-automation-tools/apps/scripts/importació/importació.py %1
