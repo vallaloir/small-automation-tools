bf3d482580748b514467d246e84fad6f3f798189
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-08-01 08:36:38 +0300

    Build sequentially with previous linting, formatting, and typing

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import subprocess
 
-# Exports the notebook as WASM to use locally or upload to GitHub Pages
-subprocess.run(
+# Formats and exports the notebook as WASM to use locally or upload to GitHub Pages
+print("Running ruff check...")
+lint = subprocess.run(
     [
         "uv",
         "run",
         "ruff",
         "check",
-        "&",
+    ]
+)
+if lint.returncode != 0:
+    exit()
+
+print("Running ruff format...")
+format_python_files = subprocess.run(
+    [
         "uv",
         "run",
         "ruff",
         "format",
-        "&",
+    ]
+)
+if format_python_files.returncode != 0:
+    exit()
+
+print("Running mypy...")
+type_check = subprocess.run(
+    [
         "uv",
         "run",
         "mypy",
         ".",
-        "--",
         "--explicit-package-bases",
         "--ignore-missing-imports",
-        "&",
+    ]
+)
+if type_check.returncode != 0:
+    exit()
+
+print("Building...")
+build = subprocess.run(
+    [
         "uv",
         "run",
         "build.py",
