e3b6442e899c230c370839f0a98b7468e78c0a4b
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-08-04 14:19:38 +0300

    Add alternative for installing uv

--- apps/public/importació.bat
+++ apps/public/importació.bat
@@ -1,3 +1,3 @@
-powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex" || py -m pip install uv
 CHCP 65001 > NUL
 uv run https://vallaloir.github.io/small-automation-tools/apps/public/importació.py %1
