5d7c31704f5c8451c2c764964463d6a839c0c19c
Anna Valldeoriola Cardó <anna.valldeoriolacardo@aalto.fi>
2025-08-06 21:04:49 +0300

    Fix timing explanation

--- apps/importació.py
+++ apps/importació.py
@@ -15,7 +15,7 @@ def _():
 def _(mo):
     CURRENT_SCRIPT = "importació"
     script_path = mo.notebook_location() / "public" / f"{CURRENT_SCRIPT}.bat"
-    DOWNLOAD_INSTRUCTIONS = f"""Descarregueu-vos [aquest fitxer]({script_path}) i arrossegueu-hi el CSV que voleu importar a Clickedu per fer-lo córrer. Cal que navegueu fins a la pàgina correcta en menys d'un minut des que s'hagi obert el navegador. Un cop el programa hagi fet els canvis i els hagueu comprovat, guardeu en menys de 15 minuts. El programa tancarà el navegador en aquest temps o abans si guardeu els canvis."""
+    DOWNLOAD_INSTRUCTIONS = f"""Descarregueu-vos [aquest fitxer]({script_path}) i arrossegueu-hi el CSV que voleu importar a Clickedu per fer-lo córrer. Cal que navegueu fins a la pàgina correcta en menys de tres minuts des que s'hagi obert el navegador. Un cop el programa hagi fet els canvis i els hagueu comprovat, guardeu en menys de 15 minuts. El programa tancarà el navegador en aquest temps o abans si guardeu els canvis."""
 
     mo.md(DOWNLOAD_INSTRUCTIONS)
     return
